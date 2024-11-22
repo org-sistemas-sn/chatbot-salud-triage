ad18e99e059034810907dc6c194fa2d908faffb4
Alfredo <alfrecanto11@gmail.com>
2024-11-22 09:53:26 -0300

    Refactor webhook route to update path for receiving messages

--- modules/Chatbot/chatbot.routes.js
+++ modules/Chatbot/chatbot.routes.js
@@ -4,7 +4,7 @@ import { healthCheck, getThreadMessages, killThread, webhook2 } from './chatbot.
 const router = Router();
 
 router.get('/health', healthCheck)
-router.post('/asdasd', webhook2)
+router.post('/messages', webhook2)
 router.get('/threads/:thread_id', getThreadMessages)
 router.delete('/threads/:thread_id', killThread)
 
