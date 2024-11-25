37f1c506c382d6e2a65da7adc9a6ee52534257f7
Alfredo <alfrecanto11@gmail.com>
2024-11-25 18:57:07 -0300

    Refactor chatbot.routes.js to remove unused test route

--- modules/Chatbot/chatbot.routes.js
+++ modules/Chatbot/chatbot.routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import { postMessage, test } from './chatbot.controller.js';
+import { postMessage } from './chatbot.controller.js';
 
 const router = Router();
 
 router.post('/messages', postMessage)
-router.post('/test', test)
 
 export default router;
